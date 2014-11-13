9b4108dd4fb8cb8c1bc49dd1d24129c4ae95dd7d
Chris Tibbs <tibbs.17@gmail.com>
2014-11-12 21:34:14 -0800

    Layout for add comment view

---
+++ app/src/main/java/com/tba/theboxingapp/AddCommentActivity.java
@@ -0,0 +1,52 @@
+package com.tba.theboxingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+
+public class AddCommentActivity extends Activity {
+
+    ImageButton mBackButton;
+    NetworkImageView userImageView;
+    TextView mNameTextView;
+    TextView mHandleTextView;
+    Button mAddCommentButton;
+    EditText mCommentContentTextView;
+    ListView mTagCandidatesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_comment);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_comment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
