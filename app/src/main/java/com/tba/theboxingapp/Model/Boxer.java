6318492b11a38f6c287fa915430bfd167e4043aa
Chris Tibbs <tibbs.17@gmail.com>
2015-08-11 20:53:54 -0700

    Extend boxer to include additional fields

--- app/src/main/java/com/tba/theboxingapp/Model/Boxer.java
+++ app/src/main/java/com/tba/theboxingapp/Model/Boxer.java
@@ -9,8 +9,19 @@ import java.util.Arrays;
  * Created by christibbs on 9/16/14.
  */
 public class Boxer extends BaseModel {
-    public String fullName;
-    public String imgUrl;
+    private String fullName;
+    private String firstName;
+    private String lastName;
+    private String imgId;
+    private String imgUrl;
+    private int wins;
+    private int losses;
+    private int draws;
+    private int knockouts;
+    private String weightClass;
+    private String heightAndWeight;
+    private String birthdate;
+    private String nationality;
 
     public Boxer(JSONObject object) {
         try {
@@ -34,6 +45,7 @@ public class Boxer extends BaseModel {
     }
 
     //<editor-fold desc="Accessors">
+
     public String getFullName() {
         return fullName;
     }
@@ -42,12 +54,101 @@ public class Boxer extends BaseModel {
         this.fullName = fullName;
     }
 
-    public String getPhotoUrl() {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(String imgId) {
+        this.imgId = imgId;
+    }
+
+    public String getImgUrl() {
         return imgUrl;
     }
 
-    public void setPhotoUrl(String imgUrl) {
+    public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getKnockouts() {
+        return knockouts;
+    }
+
+    public void setKnockouts(int knockouts) {
+        this.knockouts = knockouts;
+    }
+
+    public String getWeightClass() {
+        return weightClass;
+    }
+
+    public void setWeightClass(String weightClass) {
+        this.weightClass = weightClass;
+    }
+
+    public String getHeightAndWeight() {
+        return heightAndWeight;
+    }
+
+    public void setHeightAndWeight(String heightAndWeight) {
+        this.heightAndWeight = heightAndWeight;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
     //</editor-fold>
 }
